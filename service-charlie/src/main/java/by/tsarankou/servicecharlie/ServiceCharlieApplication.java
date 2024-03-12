c4ed6d041eb89920f277c5a3ca3e9c738dd6f94e
tsaren99 <tsaren99@sam-solutions.net>
2024-03-12 11:19:45 +0300

    add MessageListener

---
+++ service-charlie/src/main/java/by/tsarankou/servicecharlie/ServiceCharlieApplication.java
@@ -0,0 +1,13 @@
+package by.tsarankou.servicecharlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCharlieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCharlieApplication.class, args);
+    }
+
+}
---
+++ service-charlie/src/test/java/by/tsarankou/servicecharlie/ServiceCharlieApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tsarankou.servicecharlie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCharlieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
