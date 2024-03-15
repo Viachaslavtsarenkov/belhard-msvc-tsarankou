855a906df47135914e1dd8959c28d3997dce8a52
tsaren99 <tsaren99@sam-solutions.net>
2024-03-15 14:17:20 +0300

    change eureka

---
+++ resource-processor/src/main/java/by/tsarankou/resourceprocessor/config/ResourceRabbitProperties.java
@@ -0,0 +1,14 @@
+package by.tsarankou.resourceprocessor.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("broker.queue.resource")
+public class ResourceRabbitProperties {
+    private String createQueue;
+    private String topic;
+    private String createRouting;
+}
