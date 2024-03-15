85414987966a60a97dd5e387dd87a5139b92977a
tsaren99 <tsaren99@sam-solutions.net>
2024-03-15 14:14:52 +0300

    change file size

--- resource-processor/src/main/java/by/tsarankou/resourceprocessor/config/AppConfig.java
+++ resource-processor/src/main/java/by/tsarankou/resourceprocessor/config/AppConfig.java
@@ -3,6 +3,8 @@ package by.tsarankou.resourceprocessor.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 @RequiredArgsConstructor
@@ -10,7 +12,12 @@ public class AppConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        return WebClient.builder()
+                .codecs(codecs -> codecs
+                        .defaultCodecs()
+                        .maxInMemorySize(10 * 1024 * 1024))
+                .build();
     }
+
 
 }
